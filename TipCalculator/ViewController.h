7461695083e305094dd3068bfe3f3648e2ce4a53
Kimi <kiming@yahoo-inc.com>
2015-06-11 17:36:22 +0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Cheng-Yuan Wu on 6/11/15.
+//  Copyright (c) 2015 Kimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
